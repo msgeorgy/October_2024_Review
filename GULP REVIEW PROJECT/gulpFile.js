86d8c58f8f80636baa2a3d24ae7b9a09ab1193e0
msgeorgy <salebmena@yahoo.com>
2024-10-13 00:14:32 +0300

    GULP PROJECT UPDATED

---
+++ GULP REVIEW PROJECT/gulpFile.js
@@ -0,0 +1,115 @@
+var gulp = require('gulp');
+
+//let StaticServer = require('static-server');
+
+// let livereolad = require('gulp-livereload');
+
+
+let plugins = require('gulp-load-plugins')();
+
+plugins.replace = require('gulp-replace');
+
+plugins.corrector = require('gulp-line-ending-corrector')
+
+var pug = require('gulp-pug');
+
+var sass = require('gulp-sass')(require('sass'));
+
+var concat = require('gulp-concat');
+
+var cached = require('gulp-cached');
+
+var jshint = require('gulp-jshint');
+
+var sourcemaps = require('gulp-sourcemaps');
+
+let uglify = require('gulp-uglify');
+
+let plumber = require('gulp-plumber');
+
+let cleanCSS = require('gulp-clean-css');
+
+let rename = require('gulp-rename');
+
+let corrector = require('gulp-line-ending-corrector');
+
+let notify = require('gulp-notify');
+
+gulp.task('html', function() {
+
+    return  gulp.src(['./BASE-SIDE/*.pug', '!./BASE-SIDE/gallery.pug'])
+            .pipe(plumber())
+            .pipe(pug({pretty: true}))
+            .pipe(gulp.dest('./DIST/HTML/'))
+            .pipe(notify())
+
+})
+
+gulp.task('sass', function() {
+
+    return  gulp.src('./BASE-SIDE/*.scss')
+            .pipe(plumber())
+            .pipe(sass({outputStyle: 'compressed'}))
+            .pipe(gulp.dest('./BASE-TO-DIST/Compiled CSS/'))
+            .pipe(notify())
+
+})
+
+gulp.task('css', function() {
+
+    return  gulp.src('./BASE-TO-DIST/Compiled CSS/*.css')
+            .pipe(plumber())
+            .pipe(sourcemaps.init())
+            .pipe(concat('final.css'))
+            .pipe(cleanCSS())
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest('./DIST/CSS/'))
+            .pipe(notify())
+
+})
+
+gulp.task('js', function() {
+
+    return  gulp.src('./BASE-SIDE/*.js')
+            .pipe(plumber())
+            .pipe(concat('final'))
+            .pipe(cached('linting'))
+            .pipe(jshint())
+            .pipe(jshint.reporter())
+            .pipe(uglify())
+            .pipe(gulp.dest('./DIST/JS/'))
+            .pipe(notify())
+
+})
+
+gulp.task('naming', function() {
+
+    return  gulp.src('./BASE-SIDE/EXTRAS.txt')
+            .pipe(rename((text)=> {
+                text.dirname= '/MD/'
+                text.basname = 'MD_EXTRAS',
+                text.extname = '.md'
+            }))
+            .pipe(gulp.dest('./DIST/FOLDERS'))
+            .pipe(notify())
+
+})
+
+// gulp.task('rename', function() {
+
+//     return  gulp.src('./BASE-SIDE/EXTRAS.txt')
+//             .pipe(rename({prefix: 'EXES', suffix: '.md'}))
+//             .pipe(gulp.dest('./DIST/FOLDERS'))
+//             .pipe(notify())
+
+// })
+
+gulp.task('replace', function() {
+
+    return  gulp.src('./BASE-SIDE/EXTRAS.txt')
+            .pipe(plugins.replace('MSG', 'Mena Saleeb Georgy Theophilous'))
+            .pipe(plugins.corrector({verbose: true, eolc: 'CRLF'}))
+            .pipe(gulp.dest('./DIST/FOLDERS'))
+            .pipe(notify())
+
+})
